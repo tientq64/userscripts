4aaa564f1ce41d0c115ccc8a6b75fb81ad41a21d
tientq64 <tientq64@gmail.com>
2024-05-24 18:12:52 +0700

    update no-fullscreen-dropdown

---
+++ no-fullscreen-dropdown/CHANGELOG.md
@@ -0,0 +1,3 @@
+## [0.2.0] - 2024-05-24
+
+- Only toggle when pressing exactly Shift+F11.
--- no-fullscreen-dropdown/script.js
+++ no-fullscreen-dropdown/script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         No Fullscreen Dropdown
-// @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @namespace    https://github.com/tientq64/userscripts
+// @version      0.2.0
 // @description  Real fullscreen instead of fullscreen dropdown, very annoying when playing games. Useful for Microsoft Edge. Press Shift+F11 to toggle fullscreen.
 // @author       https://github.com/tientq64
 // @match        https://*
@@ -11,8 +11,10 @@
 
 window.addEventListener('keydown', (event) => {
 	if (event.repeat) return
+
 	const { key, shiftKey, ctrlKey, altKey, metaKey } = event
-	if (shiftKey && key === 'F11' && !ctrlKey && !altKey && !metaKey) {
+
+	if (key === 'F11' && shiftKey && !ctrlKey && !altKey && !metaKey) {
 		if (document.fullscreenElement) {
 			document.exitFullscreen()
 		} else {
