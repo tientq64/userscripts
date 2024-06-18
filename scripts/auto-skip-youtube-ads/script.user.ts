949bf5420d42b0bdf807ac4d190825f90262a6c5
tientq64 <tientq64@gmail.com>
2024-06-18 16:07:34 +0700

    🏗️ Increase version

--- scripts/auto-skip-youtube-ads/script.user.ts
+++ scripts/auto-skip-youtube-ads/script.user.ts
@@ -2,7 +2,7 @@
 // @name         Auto Skip YouTube Ads
 // @name:vi      Tự Động Bỏ Qua Quảng Cáo YouTube
 // @namespace    https://github.com/tientq64/userscripts
-// @version      2.0.0
+// @version      2.0.1
 // @description  Auto skip ads on YouTube. Very lightweight and efficient.
 // @description:vi  Tự động bỏ qua quảng cáo trên YouTube. Rất nhẹ và hiệu quả.
 // @author       https://github.com/tientq64
