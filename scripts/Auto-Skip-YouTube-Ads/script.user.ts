8033d6710db9495207bd6b26f89f647ca5dc69e0
tientq64 <tientq64@gmail.com>
2024-09-30 13:03:25 +0700

    Improved CSS hide ads. Clean up the code.

--- scripts/Auto-Skip-YouTube-Ads/script.user.ts
+++ scripts/Auto-Skip-YouTube-Ads/script.user.ts
@@ -10,7 +10,7 @@
 // @name:id            Lewati Otomatis Iklan YouTube
 // @name:hi            YouTube विज्ञापन स्वचालित रूप से छोड़ें
 // @namespace          https://github.com/tientq64/userscripts
-// @version            4.5.2
+// @version            4.5.3
 // @description        Automatically skip YouTube ads instantly. Remove the ad blocker warning pop-up. Very lightweight and efficient.
 // @description:vi     Tự động bỏ qua quảng cáo YouTube ngay lập tức. Loại bỏ cửa sổ bật lên cảnh báo trình chặn quảng cáo. Rất nhẹ và hiệu quả.
 // @description:zh-CN  自动立即跳过 YouTube 广告。删除广告拦截器警告弹出窗口。非常轻量且高效。
@@ -187,6 +187,7 @@ style.textContent = `
 	ytd-rich-item-renderer:has(.ytd-ad-slot-renderer),
 	ytd-reel-video-renderer:has(.ytd-ad-slot-renderer),
 	tp-yt-paper-dialog:has(#feedback.ytd-enforcement-message-view-model),
+	.ytp-suggested-action,
 	.yt-mealbar-promo-renderer {
 		display: none !important;
 	}`
@@ -199,5 +200,3 @@ document.head.appendChild(style)
 		alert(`${title}: ${config.allowedReloadPage ? 'ENABLED' : 'DISABLED'}`)
 	})
 }
-
-console.log(33)
